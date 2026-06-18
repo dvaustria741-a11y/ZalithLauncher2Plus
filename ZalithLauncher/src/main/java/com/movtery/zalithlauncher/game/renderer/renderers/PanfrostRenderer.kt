ca73ec069ad24d831d341caba12c22ddb62326ed
dvaustria741-a11y <dvaustria741@gmail.com>
2026-06-18 19:34:27 +0800

    feat: PanfrostRenderer.kt reads env from config

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/renderer/renderers/PanfrostRenderer.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/renderer/renderers/PanfrostRenderer.kt
@@ -1,37 +1,32 @@
 /*
- * Zalith Launcher 2
- * Copyright (C) 2025 MovTery <movtery228@qq.com> and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
+ * Zalith Launcher 2 Plus
  */
 
 package com.movtery.zalithlauncher.game.renderer.renderers
 
 import com.movtery.zalithlauncher.game.renderer.RendererInterface
+import com.movtery.zalithlauncher.utils.settings.PanfrostConfig
 
 object PanfrostRenderer : RendererInterface {
     override fun getRendererId(): String = "gallium_panfrost"
-
     override fun getUniqueIdentifier(): String = "9b2808c4-11af-4c72-a9c6-94c940396475"
-
+    override fun getMaxMCVersion(): String = "1.21.4"
     override fun getRendererName(): String = "Panfrost (Mali)"
 
-    override fun getMaxMCVersion(): String = "1.21.4"
-
-    override fun getRendererEnv(): Lazy<Map<String, String>> = lazy { emptyMap() }
+    override fun getRendererEnv(): Lazy<Map<String, String>> = lazy {
+        val cfg = PanfrostConfig.load() ?: PanfrostConfig()
+        buildMap {
+            put("MESA_GL_VERSION_OVERRIDE",   cfg.glVersionOverride)
+            put("MESA_GLSL_VERSION_OVERRIDE", cfg.glslVersionOverride)
+            if (cfg.noError)  put("MESA_NO_ERROR", "1")
+            put("LIBGL_MIPMAP", cfg.mipmapLevel.toString())
+            if (cfg.glThread) {
+                put("MESA_GLTHREAD", "true")
+                put("mesa_glthread",  "true")
+            }
+        }
+    }
 
     override fun getDlopenLibrary(): Lazy<List<String>> = lazy { emptyList() }
-
     override fun getRendererLibrary(): String = "libOSMesa_2300d.so"
 }
