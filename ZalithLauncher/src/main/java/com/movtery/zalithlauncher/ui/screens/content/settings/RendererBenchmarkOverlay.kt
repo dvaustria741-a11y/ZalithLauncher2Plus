42232d07ab973c85937d6e517ec490a23c9a81f0
dvaustria741-a11y <dvaustria741@gmail.com>
2026-06-25 16:34:05 +0800

    fix: add missing androidx.compose.runtime.key import

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/settings/RendererBenchmarkOverlay.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/settings/RendererBenchmarkOverlay.kt
@@ -38,6 +38,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
